c263973d09cf1220efacaef8d430eae0c97a81c0
Nina Withers <ninarosewithers@gmail.com>
2019-02-20 13:19:40 -0500

    adding new content

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Quiz from "./pages/Quiz";
-import QuizResults from "./pages/QuizResults";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import How from "./pages/How";
 import Search from "./pages/Search";
 import SearchResults from "./pages/SearchResults";
 import NavBar from "./components/NavBar";
@@ -14,6 +15,9 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/howitworks" component={How} />
           <Route exact path="/quiz" component={Quiz} />
           <Route exact path="/quiz/results" component={QuizResults} />
           <Route exact path="/search" component={Search} />
