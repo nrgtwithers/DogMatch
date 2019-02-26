fefaeed40974cdbbec75265867bd386beb988e90
Nina Withers <ninarosewithers@gmail.com>
2019-02-25 23:37:56 -0500

    adding new schema for breed for user to save breeds

---
+++ server/database/models/breeds.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BreedsSchema = new Schema({
+  breed: { type: String, required: true },
+  image: { type: String, required: true },
+  lifeSpan: { type: String, required: true },
+  weight: { type: String, required: true },
+  height: { type: String, required: true },
+  Description: { type: String, required: true },
+  UserId:{type: Schema.Types.ObjectId, ref:"User"},
+});
+
+const Breeds = mongoose.model("Breeds", BreedsSchema);
+
+module.exports = Breeds;
