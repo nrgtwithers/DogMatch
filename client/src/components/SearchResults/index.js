27f088b87ba938ebc2fe8d0307ad012d7b05bff3
Nina Withers <ninarosewithers@gmail.com>
2019-02-25 22:50:57 -0500

    Fixing up search bar with right information

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -6,7 +6,18 @@ function SearchResults(props) {
     <ul className="list-group search-results">
       {props.results.map((result,index) => (
         <li key={index} className="list-group-item">
+        <div className="row">
+        <div className="col-4 image">
           <img alt="Dog" src={result.image} className="img-fluid" />
+          </div>
+          <div className="col-8 info">
+          <h3 className="breed">{result.breed}</h3>
+          <p><strong>Life Expectancy:</strong> {result.lifeSpan}</p>
+          <p><strong>Weight:</strong> {result.weight}</p>
+          <p><strong>Height:</strong> {result.height}</p>
+          <p><strong>Description:</strong> {result.Description}</p>
+          </div>
+          </div>
         </li>
       ))}
     </ul>
