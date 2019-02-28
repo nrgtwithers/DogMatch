e7841517ee28e6df0d2e1a9ac21f605eeed0d387
Nina Withers <ninarosewithers@gmail.com>
2019-02-27 22:46:31 -0500

    changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import Logo from '../components/Logo'
 import QuizButton from '../components/QuizButton';
 import SearchBreedsBtn from '../components/SearchBreedsBtn';
 import NavBar from "../components/NavBar";
-// import NearMeButton from '../components/GoogleMap';
+import NearMeButton from '../components/GoogleMap';
 
 
 class Home extends Component {
