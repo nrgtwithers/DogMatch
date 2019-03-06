e1ef9123f71b0fd4c8d697463a929d059ddcb1d4
Fer <fgarcia@mail.wm.edu>
2019-03-06 15:08:38 -0500

    multiple results didn't work

---
+++ client/src/components/ResultsThree/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function PetfinderResults(props) {
+    return (
+        <div id="petfinderResults" >
+            {props.AdoptionResultsThree.map((result) => (
+                <li>Name: {result.name.$t} 
+                    <div id="resultImage">
+                    <img alt="dog" src={result.media.photos.photo[0].$t}/>
+                    </div>
+                    <p>Age: {result.age.$t} </p>
+                    <p>Breed: {result.breeds.breed.$t} </p>
+                    <p>Contact: {result.contact.email.$t} </p>
+                </li>
+            ))}
+        </div>
+    );
+}
+
+export default PetfinderResults;
