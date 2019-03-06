258da0e94ff9091eee78af283adebcae56768422
Fer <fgarcia@mail.wm.edu>
2019-03-06 15:08:38 -0500

    multiple results didn't work

--- client/src/pages/petfinder.js
+++ client/src/pages/petfinder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import fetchJsonp from "fetch-jsonp";
 import PetfinderResults from "../components/Results/index";
+
 // import Quiz from "./Quiz";
 
 class PetFinder extends Component {
@@ -8,8 +9,6 @@ class PetFinder extends Component {
     super(props);
     this.state = {
       AdoptionResults: [],
-      AdoptionResultsTwo: [],
-      AdoptionResultsThree:[]
     };
   }
 
@@ -24,29 +23,7 @@ class PetFinder extends Component {
      console.log(this.state.AdoptionResults)
    }))
   }
-  componentDidMount() {
-    fetchJsonp("https://api.petfinder.com/pet.find?format=json&key=e2e2583221dad933a332d4e10738bf15&location=22303&count=2&breed=Labrador Retriever", {
-      jsonpCallbackFunction: "callback"
-    })
-   .then(res => res.json())
-   .then(res=> this.setState({
-     AdoptionResultsTwo: res.petfinder.pets.pet
-   }, function(){
-     console.log(this.state.AdoptionResultsTwo)
-   }))
-  }
-
-  componentDidMount() {
-    fetchJsonp("https://api.petfinder.com/pet.find?format=json&key=e2e2583221dad933a332d4e10738bf15&location=22303&count=3&breed=Border Terrier", {
-      jsonpCallbackFunction: "callback"
-    })
-   .then(res => res.json())
-   .then(res=> this.setState({
-     AdoptionResultsThree: res.petfinder.pets.pet
-   }, function(){
-     console.log(this.state.AdoptionResultsThree)
-   }))
-  }
+  
 
   render() {
     return(
