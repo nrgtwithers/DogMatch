d10d5558dea344fdd1db115b7133f95f4b74632f
Fer <fgarcia@mail.wm.edu>
2019-03-06 13:21:59 -0500

    rendering more info

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -4,7 +4,17 @@ function PetfinderResults(props) {
     return (
         <div id="petfinderResults" >
             {props.results.map((result) => (
-                <p>Age: {result.age.$t}</p>
+                <li>Name: {result.name.$t} 
+                    <div id="resultImage">
+                    <img alt="dog" src={result.media.photos.photo[0].$t}/>
+                    </div>
+                    <p>Age: {result.age.$t} </p>
+                    <p>Breed: {result.breeds.breed.$t} </p>
+                    <p>Contact: {result.contact.email.$t} </p>
+
+                
+                </li>
+
             ))}
         </div>
     );
