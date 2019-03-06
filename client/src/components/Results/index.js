80f48358c58ff13620eaf3e71bd1e6eaffeb63c4
Fer <fgarcia@mail.wm.edu>
2019-03-06 11:59:32 -0500

    rendering name in localhost:3000/petfinder

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function PetfinderResults(props) {
     return (
-        <div id="petfinderResults" style={{ paddingRight: 50, paddingLeft: 50 }}>
+        <div id="petfinderResults" >
             {props.results.map((result) => (
-                <p>{result.address1.$t}</p>
+                <p>Name: {result.name.$t}</p>
             ))}
         </div>
     );
